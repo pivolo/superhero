839fb1ae66f51589f7a344c8b32d7f65f3bad9f0
Pablo <pivolo@gmail.com>
2021-11-27 10:25:53 +0100

    Fix junit5 conf

--- src/test/java/es/pivol/superhero/repository/SuperHeroRepositoryTI.java
+++ src/test/java/es/pivol/superhero/repository/SuperHeroRepositoryTI.java
@@ -1,18 +1,18 @@
 package es.pivol.superhero.repository;
 
 import es.pivol.superhero.model.SuperHero;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 @Sql("/data-h2.sql")
 public class SuperHeroRepositoryTI {
