d8648bc367c2a48d8706b01b8c386f7b522844cd
Pablo <pivolo@gmail.com>
2021-11-27 01:18:53 +0100

    Test para repositorio de superhero

---
+++ src/test/java/es/pivol/superhero/repository/SuperHeroRepositoryTI.java
@@ -0,0 +1,34 @@
+package es.pivol.superhero.repository;
+
+import es.pivol.superhero.model.SuperHero;
+import lombok.AllArgsConstructor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@Sql("/data-h2.sql")
+public class SuperHeroRepositoryTI {
+    //@Autowired
+    //private TestEntityManager entityManager;
+    @Autowired
+    private SuperHeroRepository sut;
+
+    @Test
+    public void findByIdTest(){
+        final long id = 1000l;
+        Optional<SuperHero> actual = sut.findById(id);
+        assertTrue(actual.isPresent(), "Id " + id + " existente");
+    }
+
+}
