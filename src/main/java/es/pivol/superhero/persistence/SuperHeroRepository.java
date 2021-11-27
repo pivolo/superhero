21cba16c5b5fd34b8daeebde47bdfce6c48bae78
Pablo <pivolo@gmail.com>
2021-11-27 10:30:52 +0100

    rename "persistence" package

--- src/main/java/es/pivol/superhero/persistence/SuperHeroRepository.java
+++ src/main/java/es/pivol/superhero/persistence/SuperHeroRepository.java
@@ -1,4 +1,4 @@
-package es.pivol.superhero.repository;
+package es.pivol.superhero.persistence;
 
 import es.pivol.superhero.model.SuperHero;
 import org.springframework.data.jpa.repository.JpaRepository;
